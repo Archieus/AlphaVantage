03bbc8ac762c84603845f65bcdbbd2fd9ea6439a
Christopher Che <cmche0221@gmail.com>
2017-06-12 14:20:32 -0400

    Convert to json and print key value pairs

--- AlphaVantage.py
+++ AlphaVantage.py
@@ -2,4 +2,11 @@ import json
 import requests
 rawdata = requests.get('http://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&outputsize=compact&apikey=Y474')
 
-print (rawdata.json())
+data = json.loads(rawdata.text)
+
+print(data)
+
+inner_dict = data['Time Series (Daily)']
+
+for key in inner_dict.keys():
+    print('{}: {}'.format(key, inner_dict[key]))

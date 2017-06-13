0f86e04a961f373fe562c6aa86f553409d92b11c
Christopher Che <cmche0221@gmail.com>
2017-06-13 19:36:00 -0400

    Using close values now

--- AlphaVantage.py
+++ AlphaVantage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import json
 import requests
-from scipy import stats
 import numpy as np
 from sklearn import linear_model
+from scipy import stats
 
-json_keys = ('Time Series (Daily)', '1. open')
+json_keys = ('Time Series (Daily)', '4. close')
 
 symbol1 = input("Enter a stock symbol\n")
 symbol2 = input("Enter another stock symbol\n")
@@ -20,24 +20,24 @@ data2 = json.loads(rawdata2.text)
 inner_dict1 = data1[json_keys[0]]
 inner_dict2 = data2[json_keys[0]]
 
-open_values1 = []
-open_values2 = []
+values1 = []
+values2 = []
 
 for key in sorted(inner_dict1.keys()):
-    open_values1.append((key.replace('-', '').split(' ')[0], inner_dict1[key][json_keys[1]]))
+    values1.append((key.replace('-', '').split(' ')[0], inner_dict1[key][json_keys[1]]))
 
 for key in sorted(inner_dict2.keys()):
-    open_values2.append((key.replace('-', '').split(' ')[0], inner_dict2[key][json_keys[1]]))
+    values2.append((key.replace('-', '').split(' ')[0], inner_dict2[key][json_keys[1]]))
 
 a = []
-for i in range(len(open_values1)):
-    a.append([open_values1[i][1], open_values2[i][1]])
+for i in range(len(values1)):
+    a.append([values1[i][1], values2[i][1]])
 
 a = np.array(a)
-b = np.array(list(x[0] for x in open_values1))
+b = np.array(list(x[0] for x in values1))
 
 a.reshape(len(a), 2)
-b.reshape(len(open_values2), 1)
+b.reshape(len(values2), 1)
 
 a = a.astype(np.float).tolist()
 b = b.astype(np.int).tolist()
@@ -45,8 +45,8 @@ b = b.astype(np.int).tolist()
 clf = linear_model.LinearRegression()
 clf.fit(a, b)
 
-foo1 = [float(x[1]) for x in open_values1]
-foo2 = [float(x[1]) for x in open_values2]
+foo1 = [float(x[1]) for x in values1]
+foo2 = [float(x[1]) for x in values2]
 
 slope, intercept, r_value, p_value, std_err = stats.linregress(foo1, foo2)
 print(slope, intercept)
